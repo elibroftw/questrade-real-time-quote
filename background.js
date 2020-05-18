273010ea1a9ef4ce94cc2f41d1b06db4df12e7c1
Elijah Lopez <elijahllopezz@gmail.com>
2020-05-18 16:07:15 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
 // TODO: add delay option (seconds)
 // TODO: add options_ui
 
+var MS_DELAY = 1000;
+
 function clickBtns() {
     btns = document.getElementsByClassName('snap-timer');
     [].forEach.call(btns, btn => {
         btn.click();
     });
     if (document.hasFocus()) {
-        window.setTimeout(clickBtns, 1000);
+        window.setTimeout(clickBtns, MS_DELAY);
     } else {
         document.onfocus = clickBtns;
     }   
