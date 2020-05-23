34e98de48525e78500a661cd97a2400b284bf143
Elijah Lopez <elijahllopezz@gmail.com>
2020-05-22 23:26:04 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,9 +5,10 @@ var MS_DELAY = 1000;
 
 function clickBtns() {
     btns = document.getElementsByClassName('snap-timer');
-    [].forEach.call(btns, btn => {
+    for (const btn of btns) {
         btn.click();
-    });
+    }
+    
     if (document.hasFocus()) {
         if (typeof browser !== "undefined") {
             window.setTimeout(clickBtns, MS_DELAY);
